cc1546373354cc431ad59da2e9153b86c08e0844
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-05-02 14:16:36 +0200

    Add async to tangle org config

--- init.el
+++ init.el
@@ -1,8 +1,15 @@
 ;;; init.el --- Emacs configuration.
 
+;; Make startup faster by reducing the frequency of garbage
+;; collection.
+(setq gc-cons-threshold (* 50 1000 1000))
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (package-initialize)
 (load-file (concat user-emacs-directory "config.el"))
 (put 'narrow-to-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
