7cdf4c7272c470218a46a1ec4475e871dc50df30
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-05-14 17:18:29 +0200

    Use :preface for dashboard

--- config.el
+++ config.el
@@ -59,14 +59,14 @@
   :init (dumb-jump-mode)
   :custom (dump-jump-selector 'ivy))
 
-(defun my/dashboard-banner ()
-  """Set a dashboard banner including information on package initialization
-   time and garbage collections."""
-  (setq dashboard-banner-logo-title
-        (format "Emacs ready in %.2f seconds with %d garbage collections."
-                (float-time (time-subtract after-init-time before-init-time)) gcs-done)))
-
 (use-package dashboard
+  :preface
+  (defun my/dashboard-banner ()
+    "Set a dashboard banner including information on package initialization
+  time and garbage collections."""
+    (setq dashboard-banner-logo-title
+          (format "Emacs ready in %.2f seconds with %d garbage collections."
+                  (float-time (time-subtract after-init-time before-init-time)) gcs-done)))
   :init
   (add-hook 'after-init-hook 'dashboard-refresh-buffer)
   (add-hook 'dashboard-mode-hook 'my/dashboard-banner)
